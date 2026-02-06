7421f7916f3cad6960488c4fe13f774c3453cab0
michael.remetwa@polikant.ac.id <michael.remetwa@polikant.ac.id>
2026-02-06 09:50:11 +0900

    Menambah Formulir

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ tombol.addEventListener('click', function() {
     // Memberitahu body untuk ganti "class"
     document.body.classList.toggle('dark-mode');
 });
+
+const btnKirim = document.getElementById('btn-kirim');
+btnKirim.onclick = function(event) {
+    event.preventDefault(); // Mencegah halaman refresh
+    alert("Terima kasih, " + document.querySelector('input[type="text"]').value + "! Pesan kamu sudah terkirim (simulasi).");
+};
